95c90e03f9c58ba337c8d94e3c5a465f7249b590
Dan Obermiller <dannnno16@gmail.com>
2015-01-16 11:12:28 -0600

    New computer
    Things don't work on my new computer. Fantastic.

--- submission_site/sub_site/handle_database.py
+++ submission_site/sub_site/handle_database.py
@@ -14,11 +14,10 @@ def _connect_to_database():
 
 
 def init_db():
-    if not os.path.exists(app.config['DATABASE']):
-        with closing(_connect_to_database()) as db:
-            with app.open_resource(app.config['SCHEMA'], mode='r') as f:
-                db.cursor().executescript(f.read())
-            db.commit()
+    with closing(sqlite3.connect(app.config['DATABASE'])) as db:
+        with app.open_resource(app.config['SCHEMA'], mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 @app.before_request
